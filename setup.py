b4ac79cd02c459e569290a4857535826cb42e17f
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-03-29 15:24:45 -0500

    license added

--- setup.py
+++ setup.py
@@ -4,16 +4,20 @@ from distutils.core import setup
 setup(
     name="remio",
     version="0.2.0",
-    description="Python remIO",
+    description="A library for managing concurrent socketio, cv2, and pyserial processes. Useful for making robots or devices with Arduinos and Raspberry Pi.",
+    license = "Apache License 2.0",
     author="Jason Macas",
     author_email="franciscomacas3@gmail.com",
     install_requires=[i.strip() for i in open("./requirements.txt").readlines()],
     keywords=[
         "OpenCV",
         "Serial",
-        "SocketIO" "multithreading",
+        "SocketIO",
+        "multithreading",
         "multiprocessing",
         "IoT",
+        "mjpeg",
+        "Arduino"
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -24,6 +28,7 @@ setup(
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Education",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",

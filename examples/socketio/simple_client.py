58317a6f33ef4f147c55dcc962e86430342578aa
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-04-10 11:29:11 -0500

    socketio examples updated

---
+++ examples/socketio/simple_client.py
@@ -0,0 +1,19 @@
+"""Simple socketio client."""
+import time
+from remio import CustomSocketIO
+
+# Intialize the socketio client
+socket = CustomSocketIO(address='http://localhost:5000')
+
+# Define routes
+socket.on("connection", lambda: print(f"-> connected: {socket.isConnected()}"))
+socket.on("message", lambda response: print(f"- server says: {response}"))
+
+# run the socketio client
+socket.start()
+
+# Emit some message to the server
+time.sleep(1)
+message = "Hi, I'm a python socketio client."
+print(f"- client says: {message}")
+socket.emit("message", message)
---
+++ examples/socketio/simple_server.py
@@ -0,0 +1,28 @@
+"""Simple socketio server for test socketio client."""
+import eventlet
+import socketio
+
+# Initialize server app
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+# Define events
+@sio.event
+def connect(sid, environ):
+    print(f'-> connect: {sid}')
+
+# Receive a message from the client, and response to it
+@sio.event
+def message(sid, data):
+    print(f'-> message from {sid}: {data}')
+    sio.emit('message', 'Welcome.')
+
+# If some client disconnect
+@sio.event
+def disconnect(sid):
+    print('disconnect: ', sid)
+
+
+if __name__ == '__main__':
+    # export EVENTLET_HUB=poll -> export this if you have some trouble starting server
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
